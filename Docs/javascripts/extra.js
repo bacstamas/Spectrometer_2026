23c51b9d56b37ffba35ab1d81e476d5c852ffd6b
Bács Tamás <bacstamas01@gmail.com>
2026-05-07 01:59:22 +0300

    Trying to make the javadoc open in new tab

--- Docs/javascripts/extra.js
+++ Docs/javascripts/extra.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var links = document.querySelectorAll("a");
-    for (var i = 0; i < links.length; i++) {
-        // Check if the link contains 'JavaDocs' or matches the exact name
-        if (links[i].href.includes("JavaDocs/index.html")) {
-            links[i].target = "_blank";
-            links[i].rel = "noopener";
-        }
+/* This script listens for MkDocs' internal page transitions */
+app.document$.subscribe(function() {
+  var links = document.querySelectorAll("a");
+  links.forEach(function(link) {
+    if (link.href.includes("JavaDocs/index.html")) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener");
     }
+  });
 });
